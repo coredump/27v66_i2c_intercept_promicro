dd6a41c63eb65c0112cd39381f6709405c4cc22e
Jose Junior <jose.junior@gmail.com>
2021-08-06 22:45:45 -0400

    Let's try without ic2

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 
 #include <Arduino.h>
 #include <Wire.h>
+#include <RingBufCPP.h>
+#include <singleLEDLibrary.h>
 
 #define DELAY_MS 100
 
@@ -13,10 +15,9 @@
 #define SDA_PIN 5 // PB5 - Pin 9 on a Pro Micro
 #define SDA_PORT PORTB
 #define I2C_PULLUP 0
-#define I2C_FASTMODE 1
+#define I2C_FASTMODE 0
 
 #include <SoftI2CMaster.h>
-#include <Wire.h>
 
 // Address in the datasheet is said to be 0x88 for write
 // and 0x89 for read. That is somewhat of a "mistake" as
@@ -24,129 +25,119 @@
 // is read (1) or write (0). Thus the address is shifted once
 // to the right to get the "real" address which is then 44h/68
 #define JUNGLE_ADDR (0x44)
+
 /* For the CXA2133S this seems to work
    There are no datasheets for it, but the CXA2061S seems to be 
    very close.
 */
 #define REG_RGB (0xA)
+#define REG_INIT (0x9)
+
+/* Easier than writing 11111110 all the time */
+#define MASK (0xfe)
+
+/* Theoretically this will be enough (famous last words) */
+#define BUFFER_SIZE (32)
 
 void writeRequest(int);
 void readRequest();
 
+RingBufCPP<int, BUFFER_SIZE> buf;
+sllib led(LED_BUILTIN_RX);
+
 uint8_t r[2] = {0, 0};
 unsigned long start = millis();
 bool iicinit = false;
-bool masked = false;
 
 void setup()
 {
   Serial.begin(115200);
   Serial.print("Sony Jungle I2C Bridge\n");
+  led.setBlinkSingle(1000);
+
   iicinit = i2c_init();
   Wire.setClock(100000);
-  Wire.begin(JUNGLE_ADDR);
   Wire.onReceive(writeRequest);
   Wire.onRequest(readRequest);
-  pinMode(LED_BUILTIN, OUTPUT);
+  Wire.begin(JUNGLE_ADDR);
+  pinMode(LED_BUILTIN_RX, OUTPUT);
 
   if (!iicinit)
   {
     Serial.println("I2C init failed");
+    int pattern[] = {1000, 200, 500, 200, 500, 200};
+    led.setPatternSingle(pattern, 6);
   }
 }
 
-void writeRegister(const uint8_t reg, const uint8_t val)
-{
-  i2c_start((JUNGLE_ADDR << 1) | I2C_WRITE);
-  i2c_write(reg);
-  i2c_write(val);
-  i2c_stop();
-}
-
-void writeRequest(int byteCount)
-{
-  if (byteCount > 2)
-  {
-    uint8_t reg = Wire.read();
-    uint8_t bc = 1;
-    i2c_start((JUNGLE_ADDR << 1) | I2C_WRITE);
-    i2c_write(reg);
-    do
-    {
-      uint8_t val = Wire.read();
-      switch (reg)
-      {
-      case REG_RGB:
-        // mask the value to make the last bit 0 without changing anything else
-        val = (val & 0b11111110);
-        masked = true;
-        break;
-      default:
-        break;
-      }
-      i2c_write(val);
-      ++bc;
-      ++reg;
-    } while (bc < byteCount);
-    i2c_stop();
-  }
-  else
-  {
-    uint8_t reg = Wire.read();
-    uint8_t val = Wire.read();
-    switch (reg)
-    {
-    case REG_RGB:
-      writeRegister(reg, (val & 0b11111110));
-      masked = true;
-      break;
-    default:
-      writeRegister(reg, val);
-      break;
-    }
-  }
-}
+// void writeRegister(const uint8_t reg, const uint8_t val)
+// {
+//   i2c_start((JUNGLE_ADDR << 1) | I2C_WRITE);
+//   i2c_write(reg);
+//   i2c_write(val);
+//   i2c_stop();
+// }
 
 void readRequest()
 {
   Wire.write(r, 2);
 }
 
-void read()
+void writeRequest(int byteCount)
 {
-  i2c_start((JUNGLE_ADDR << 1) | I2C_READ);
-  r[0] = i2c_read(false); // read one byte
-  r[1] = i2c_read(true);  // read one byte and send NAK to terminate
-  i2c_stop();             // send stop condition
+  bool reg_found = false;
+  bool single_reg_found = false;
+  int c = 0;
+  int data = 0;
+
+  while (Wire.available())
+  {
+    data = Wire.read();
+    if (byteCount > 2)
+    {
+      if (data == REG_INIT)
+      {
+        reg_found = true;
+      }
+
+      if (reg_found && (c == 2))
+      {
+        data = data & MASK;
+      }
+      c++;
+    }
+    else if (byteCount == 2)
+    {
+      if (data == REG_RGB)
+      {
+        single_reg_found = true;
+      }
+
+      if (single_reg_found && Wire.available() == 0)
+      {
+        data = data & MASK;
+      }
+    }
+    buf.add(data);
+  }
 }
 
 void loop()
 {
-  noInterrupts();
-  read();
-  interrupts();
-  delay(DELAY_MS);
-  if ((millis() - start) >= 1000)
+  int d;
+  if (buf.numElements() > 0)
   {
-    Serial.println("Alive");
-    if (!iicinit)
-    {
-      Serial.println("IICinit failed!");
+    if(!i2c_start(JUNGLE_ADDR << 1 | I2C_WRITE)) {
+      led.setBlinkSingle(100);
     }
-    else
-    {
-      Serial.println("IICinit succeeded");
+    else {
+      led.setBlinkSingle(1000);
     }
-
-    if (masked)
+    while (buf.pull(&d))
     {
-      Serial.println("Masked is true");
-      masked = false;
-    } else
-    {
-      Serial.println("masked is false");
+      i2c_write(d);
     }
-    
-    start = millis();
+    i2c_stop();
   }
+  led.update();
 }
